7ef453e60b3c26d86f4f9a7de8d3070dbc013bf3
Ariel Cuesta <arielcc88@gmail.com>
2020-08-04 19:01:58 -0500

    corrected signup verification functions. extracting roles from DB.

--- routes/auth_router.js
+++ routes/auth_router.js
@@ -1,20 +1,21 @@
 //Importing helping functions from utils
 //===========================================
 const VerifySignUp = require("../utils/fnVerifySignUp");
-const JwtTokenValidator = require("../utils/fnVerifyJwtToken");
+//const JwtTokenValidator = require("../utils/fnVerifyJwtToken");
 
 //auth routes
 module.exports = (app) => {
     //including controller here to be exported
-    const authController = require("../controllers/auth_controller");
+    //const authController = require("../controllers/auth_controller");
 
     //defining route for signup
-    app.post("/api/auth/signup", [VerifySignUp.fnCheckDuplicateUserOrEmail, VerifySignUp.fnCheckRoles], authController.signup);
+    //app.post("/api/auth/signup", [VerifySignUp.fnCheckDuplicateEmail, VerifySignUp.fnCheckRoles], authController.signup);
+    app.post("/api/auth/signup", [VerifySignUp.fnCheckDuplicateEmail, VerifySignUp.fnCheckRoles], () => {console.log("Next Function here!")});
 
     //defining route for signin
-    app.post("/api/auth/signin", authController.signin)
+    //app.post("/api/auth/signin", authController.signin)
 
     //Defining test routes
-    app.get('/api/test/student', [JwtTokenValidator.fnVerifyToken], authController.studentContent);
-    app.get('/api/test/admin', [JwtTokenValidator.verifyToken, JwtTokenValidator.isAdmin], authController.adminBoard);
+    //app.get('/api/test/student', [JwtTokenValidator.fnVerifyToken], authController.studentContent);
+    //app.get('/api/test/admin', [JwtTokenValidator.verifyToken, JwtTokenValidator.isAdmin], authController.adminBoard);
 }
