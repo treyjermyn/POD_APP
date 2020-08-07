3b7f871de88bd6a6334a36a084a59bc804a95b84
Samir Bello <agnide4@gmail.com>
2020-08-06 20:52:24 -0500

    Created user srudent and instructor classes

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,17 @@ const capital = document.getElementById("capital");
 const number = document.getElementById("number");
 const length = document.getElementById("length");
 
+//Class person for use all over the app
+class User {
+    constructor(fname, lname, email){
+      this.fname = fname;
+      this.lname = lname;
+      this.email = email;
+    }
+
+}
+
+
 //lessons content filtering
 // shows and hides filtered items
 $(".filter-simple-button").click(function() {
@@ -138,4 +149,7 @@ $(".filter-simple-button").click(function() {
   function newStudent() {
 
   }
+
+
+  module.exports = User
   
